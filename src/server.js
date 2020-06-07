f9a1ea49740ff7ee015185b0079ad913829b27b1
Talita Monteiro <tfpmonteiro@outlook.com>
2020-06-07 16:32:09 -0300

    Aula4
    Criação do server através do node e instação do nunjucks

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const server = express();
+
+//configurar a pasta public
+server.use(express.static("public"))
+
+//utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+//Configurando o caminho da página inicial
+server.get("/", (req, res) => {
+    return res.render("index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search-results", (req, res) => {
+    return res.render("search-results.html")
+})
+
+//iniciar o server
+server.listen(3000);
